62613f5c72b2c05b9e7adad79eb78257eedfa6a7
volodymyrmigdal <volodymyr.migdal@gmail.com>
2017-09-29 21:48:43 +0300

    adjust travis, package, add sample

---
+++ sample/test.js
@@ -0,0 +1,50 @@
+( function _Test_test_s_( ) {
+
+  'use strict';
+
+  var _ = wTools;
+
+  //
+
+  var simplest = function( test )
+  {
+
+    test.description = 'colorNameNearest bitmask';
+    var color = 0xff0000;
+    var got = _.color.rgbaFrom( color );
+    var expected = [ 1,0,0,1 ];
+    test.identical( got,expected );
+
+    test.description = 'colorNameNearest name';
+    var color = 'red';
+    var got = _.color.rgbaFrom( color );
+    var expected = [ 1,0,0,1 ];
+    test.identical( got,expected );
+
+  }
+
+  //
+
+  var Self =
+  {
+
+    name : 'Test',
+    silencing : 1,
+    verbosity : 5,
+
+    tests :
+    {
+      simplest : simplest,
+    },
+
+  }
+
+  //
+
+  Self = wTestSuite( Self );
+  if( typeof module !== 'undefined' && !module.parent )
+  _.Tester.test( Self.name );
+
+  } )( );
+
+
